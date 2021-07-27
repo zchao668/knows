b0f0f1a57e5990f964fbcd1a562ccfa8e2a85b6e
zchao668 <zchao668@163.com>
2021-07-27 16:14:25 +0800

    2-2 init

---
+++ src/main/java/com/work/knows/KnowsApplication.java
@@ -0,0 +1,13 @@
+package com.work.knows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/work/knows/KnowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.knows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
