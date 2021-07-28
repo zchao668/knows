43bac6f5691935b2186f53c78cb5f5829654be78
zchao668 <zchao668@163.com>
2021-07-28 14:52:57 +0800

    2-4 增加hello接口测试test.http

--- src/main/java/com/work/knows/controller/HelloController.java
+++ src/main/java/com/work/knows/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.work.knows.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
-    //get put 浏览器查询只支持get
+    //get put request delete 浏览器查询只支持get
+//    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String hello1(String name){
+        return "hello world" + name;
     }
 }
