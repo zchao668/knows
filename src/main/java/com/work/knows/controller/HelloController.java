7209dd5934e12f39304c70f461c82aefefbf0020
zchao668 <zchao668@163.com>
2021-07-28 15:11:09 +0800

    2-8 增加了自定义配置项示例

--- src/main/java/com/work/knows/controller/HelloController.java
+++ src/main/java/com/work/knows/controller/HelloController.java
@@ -1,15 +1,19 @@
 package com.work.knows.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
+    //取得配置文件中的自定义属性,若没写，则默认：后面的内容
+    @Value("${test.hello:xixixi}")
+    private String testHello;
 
     //get put request delete 浏览器查询只支持get
-//    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    //@RequestMapping(value = "/hello",method = RequestMethod.GET)
     @RequestMapping("/hello")
     public String hello(){
-        return "hello World";
+        return "hello World" + testHello;
     }
 
     @PostMapping("/hello/post")
