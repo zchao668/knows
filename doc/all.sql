46ee1eef7e79784207afdd638b3ce764d6ee2e2c
zchao668 <zchao668@163.com>
2021-07-28 20:23:48 +0800

    2-9 增加sql文件

---
+++ doc/all.sql
@@ -0,0 +1,6 @@
+create table 'test'(
+    'id' int not null comment 'id',
+    'name' varchar(50) comment '名称',
+    'password' varchar(50) comment '密码',
+    primary key ('id')
+) engine=innodb default charset =utf8mb4 comment = '测试';
