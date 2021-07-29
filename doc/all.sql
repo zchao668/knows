fdbc84adf28593f32fd52450a3ebe1be1df3c7e9
zchao668 <zchao668@163.com>
2021-07-29 14:34:58 +0800

    3-5 新增ebook表，并且生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -11,3 +11,27 @@ create table demo(
                      name varchar(50) comment '名称',
                      primary key (id)
 ) engine=innodb default charset =utf8mb4 comment = '测试';
+
+drop table if exists ebook;
+create table ebook(
+                     id bigint not null comment 'id',
+                     name varchar(50) comment '名称',
+                     category1_id bigint comment '分类1',
+                     category2_id bigint comment '分类2',
+                     description varchar(200) comment '名称',
+                     cover varchar(200) comment '封面',
+                     doc_count int comment '文档数',
+                     view_count int comment '阅读数',
+                     vote_count int comment '点赞数',
+                     primary key (id)
+) engine=innodb default charset =utf8mb4 comment = '电子书';
+
+insert into ebook(id,name,description) values (1,'Springboot 入门','零基础学习Java开发');
+insert into ebook(id,name,description) values (2,'Vue 入门','零基础学习Vue开发');
+insert into ebook(id,name,description) values (3,'LInux 入门','零基础学习Linux开发');
+
+
+
+
+
+
