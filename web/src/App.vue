c32e31ae5a508b617ee48a6da2bef622fcaf12ad
zchao668 <zchao668@163.com>
2021-07-30 14:31:32 +0800

    4-7 制作the-header 组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,18 +1,6 @@
 <template>
     <a-layout>
-        <a-layout-header class="header">
-            <div class="logo" />
-            <a-menu
-                    theme="dark"
-                    mode="horizontal"
-                    v-model:selectedKeys="selectedKeys1"
-                    :style="{ lineHeight: '64px' }"
-            >
-                <a-menu-item key="1">nav 1</a-menu-item>
-                <a-menu-item key="2">nav 2</a-menu-item>
-                <a-menu-item key="3">nav 3</a-menu-item>
-            </a-menu>
-        </a-layout-header>
+            <the-header></the-header>
             <router-view></router-view>
         <a-layout-footer style="text-align: center">
             Knows 知识库
@@ -20,22 +8,13 @@
     </a-layout>
 </template>
 <script lang="ts">
-    import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
-    import { defineComponent, ref } from 'vue';
+    import { defineComponent } from 'vue';
+    import TheHeader from '@/components/the-header.vue'; // @ is an alias to /src
     export default defineComponent({
-        components: {
-            UserOutlined,
-            LaptopOutlined,
-            NotificationOutlined,
-        },
-        setup() {
-            return {
-                selectedKeys1: ref<string[]>(['2']),
-                selectedKeys2: ref<string[]>(['1']),
-                collapsed: ref<boolean>(false),
-                openKeys: ref<string[]>(['sub1']),
-            };
-        },
+        name: 'app',
+        components:{
+            TheHeader
+        }
     });
 </script>
 <style>
