b8dc8b785e13c163e774f9c81ca54e090658bb0b
zchao668 <zchao668@163.com>
2021-07-31 09:38:22 +0800

    5-8 增加拦截器，打印接口耗时

---
+++ src/main/java/com/work/knows/config/SpringMvcConfig.java
@@ -0,0 +1,20 @@
+package com.work.knows.config;
+
+import com.work.knows.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**");
+    }
+}
