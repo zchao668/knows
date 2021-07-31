6c5304be23a48dd84c38f46fe52cfe3dbe1e2643
zchao668 <zchao668@163.com>
2021-07-31 09:14:10 +0800

    5-6 配置axios拦截器 打印请求参数和返回参数

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -99,12 +99,10 @@
                 const ebooks1 = reactive({books: []});
 
                 onMounted(() => {
-                    console.log('onMounted');
                     axios.get("/ebook/list").then((response) => {
                         const data = response.data;
                         ebooks.value = data.content;
                         ebooks1.books = data.content;
-                        console.log(response);
                     });
                 })
 
